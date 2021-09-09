888603cc29a8c5a72da08e3b857aedc2c88f6207
junior-ux <55417250+junior-ux@users.noreply.github.com>
2021-09-09 00:40:37 -0300

    conexão com a API

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -8,11 +8,28 @@ class News {
 
   Future<void> getNews() async {
     var url = Uri.https(
-        'https://newsapi.org/v2/everything?q=Apple&from=2021-09-08&sortBy=popularity&apiKey=API_KEY',
+        'https://newsapi.org/v2/everything?q=tesla&from=2021-08-08&sortBy=publishedAt&apiKey=6767a5df034a4f4f8b669633fc2916f1',
         '/books/v1/volumes',
         {'q': '{http}'});
 
     var response = await http.get(url);
     var jsonData = jsonDecode(response.body);
+    //verificando se os dados estão nulos antes da exibição
+    if (jsonData['status'] == 'ok') {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+            author: element['author'],
+            title: element['title'],
+            description: element['description'],
+            url: element['url'],
+            urlToImage: element['urlToImage'],
+            content: element['context'],
+            publishedAt: element['publishedAt'],
+          );
+          news.add(articleModel);
+        }
+      });
+    }
   }
 }
